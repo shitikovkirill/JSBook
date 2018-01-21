c16ead1edbd36ae86e4b5e7790691f06e1954f85
Kirill <sh.kiruh@gmail.com>
2018-01-21 18:43:35 +0200

    Add for of

---
+++ new_in_es6/src/9_for_of.js
@@ -0,0 +1,9 @@
+var a = ['a', 'b', 'c', 'd', 'e'];
+
+for (var idx in a) {
+    // ...
+}
+
+for (var val of a) {
+    // ...
+}
---
+++ new_in_es6/test/9_for_of_test.js
@@ -0,0 +1,17 @@
+QUnit.test( "Arrow function: this call applay", function( assert ) {
+    var a = ['a', 'b', 'c', 'd', 'e'];
+
+    var temp1 = [];
+    for (var idx in a) {
+        temp1.push(idx)
+    }
+    assert.deepEqual(temp1, ["0","1","2","3","4"]);
+
+    var temp2 = [];
+    for (var val of a) {
+        temp2.push(val)
+    }
+
+    assert.deepEqual(temp2, ['a','b','c','d','e']);
+});
+
